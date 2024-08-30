4a205eb5383f4dca5b7b93ee285af3c28bda7a99
rodnei-c <rmcaetano04@gmail.com>
2024-08-30 13:37:25 -0300

    prova 1

---
+++ rodnei-caetano-prova1/src/main/java/com/example/rodnei_caetano_prova1/RodneiCaetanoProva1Application.java
@@ -0,0 +1,13 @@
+package com.example.rodnei_caetano_prova1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodneiCaetanoProva1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodneiCaetanoProva1Application.class, args);
+	}
+
+}
---
+++ rodnei-caetano-prova1/src/test/java/com/example/rodnei_caetano_prova1/RodneiCaetanoProva1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rodnei_caetano_prova1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodneiCaetanoProva1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
